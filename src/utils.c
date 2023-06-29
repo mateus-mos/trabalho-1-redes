b7bbcdf1fcfc432590da35f16d93c672037a5424
Leo <leowork2002@gmail.com>
2023-06-29 18:46:46 -0300

    update

--- src/utils.c
+++ src/utils.c
@@ -160,7 +160,7 @@ char *get_file_path(char *log_file, char *file_name) {
         if (delimiter) {
             char *file_path = (char *)malloc(strlen(delimiter + 1) + 1);
             strcpy(file_path, delimiter + 1);
-            file_path[strlen(delimiter) + 1] = '\0';
+            file_path[strlen(delimiter)] = '\0';
             return file_path;
         }
     }
